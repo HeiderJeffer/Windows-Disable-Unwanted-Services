2b310fce5c49502ae47724c9817d240c5375c021
Heider Jeffer <hheider.jeffer@gmail.com>
2023-04-20 05:03:03 +0200

    bat files

---
+++ Disabel Remote and Hyper Services.bat
@@ -0,0 +1,62 @@
+sc config "RasAuto" start= disabled
+sc config "RasMan" start= disabled
+sc config "SessionEnv" start= disabled
+sc config "TermService" start= disabled
+sc config "UmRdpService" start= disabled
+sc config "RpcSs" start= disabled
+sc config "RpcLocator" start= disabled
+sc config "RemoteRegistry" start= disabled
+sc config "vmickvpexchange" start= disabled
+sc config "vmicguestinterface" start= disabled
+sc config "vmicshutdown" start= disabled
+sc config "vmicheartbeat" start= disabled
+sc config "vmicvmsession" start= disabled
+sc config "vmicrdv" start= disabled
+sc config "vmictimesync" start= disabled
+sc config "vmicvss" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+sc config "Name of Service" start= disabled
+
+
+
+
+
+
+
+
+
+
+
+
+
+
